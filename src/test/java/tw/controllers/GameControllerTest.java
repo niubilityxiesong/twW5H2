111faae5f160ef91df61ab4b7bc9a04dd8da944a
niubilityxiesong <563527588@qq.com>
2018-04-20 18:30:52 +0800

    try1

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -1,8 +1,54 @@
 package tw.controllers;
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.commands.GuessInputCommand;
+import tw.core.Answer;
+import tw.core.Game;
+import tw.core.generator.AnswerGenerator;
+import tw.core.generator.RandomIntGenerator;
+import tw.views.GameView;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+
 /**
  * 在GameControllerTest文件中完成GameController中对应的单元测试
  */
 public class GameControllerTest {
+
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setup() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    private String systemOut() {
+
+        return outContent.toString();
+    }
+
+    @Test
+    public void testbegingame() throws Exception {
+
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+        Game game = new Game(answerGenerator);
+        GameView gameView = new GameView();
+
+        GameController gameController = new GameController(game, gameView);
+        gameController.beginGame();
+
+        assertThat(systemOut()).isEqualTo("------Guess Number Game, You have 6 chances to guess!  ------\n");
+    }
+
+
 
 }
