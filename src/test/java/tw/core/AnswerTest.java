59ffb6783ac074d0cb2163aa66fbc21a9c6d84d3
niubilitisong <66543775xie>
2018-04-25 18:31:42 +0800

    finish answertest

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,8 +1,56 @@
 package tw.core;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.model.Record;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
  */
 public class AnswerTest {
 
+    private String usearray = "1 2 3 4";
+
+    @Mock
+    Answer mockanswer;
+
+    @Before
+    public void steup(){
+
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void test_creatAnswer() throws Exception{
+
+        Answer answer = new Answer();
+
+        String result = answer.createAnswer(usearray).toString();
+
+        assertThat(result).isEqualTo("1 2 3 4");
+    }
+
+    @Test(expected = OutOfRangeAnswerException.class)
+    public void test_validata() throws Exception{
+
+        Answer answer = Answer.createAnswer("1 2 3 44");
+
+        answer.validate();
+    }
+
+    @Test
+    public void test_check_should_return_11() throws Exception{
+
+        Answer actualanswer = Answer.createAnswer("1 2 3 4");
+        Answer inputanswer = Answer.createAnswer("1 6 7 2");
+        Record value = actualanswer.check(inputanswer);
+        int[] rightvalue = {1,1};
+        assertThat(value.getValue()).isEqualTo(rightvalue);
+    }
 }
--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,58 @@
 package tw.core.generator;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import tw.core.Answer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static com.sun.javaws.JnlpxArgs.verify;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
 
+    RandomIntGenerator randomIntGenerator;
+    AnswerGenerator answerGenerator;
+
+    @Mock
+    RandomIntGenerator mockrandom;
+
+    @Before
+    public void setup() throws Exception{
+
+        randomIntGenerator = new RandomIntGenerator();
+        MockitoAnnotations.initMocks(this);
+        answerGenerator = new AnswerGenerator(mockrandom);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_return_IllegalArgumentException() throws Exception{
+
+        randomIntGenerator.generateNums(4,9);
+    }
+
+    @Test
+    public void should_return_length_of_randomIntGenerator() throws Exception{
+
+        assertThat(randomIntGenerator.generateNums(9,4).length()).isEqualTo(7);
+    }
+
+    @Test
+    public void should_return_array_of_answer() throws Exception{
+
+        when(mockrandom.generateNums(9,4)).thenReturn("1 2 3 4");
+
+        Answer answer = answerGenerator.generate();
+
+        assertThat(answer.toString()).isEqualTo("1 2 3 4");
+
+    }
 }
 
