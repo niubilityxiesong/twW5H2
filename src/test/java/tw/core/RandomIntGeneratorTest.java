f507cf33fe276a16e0c9c0092e47fec7dd6906a4
niubilitisong <66543775xie>
2018-04-25 19:35:16 +0800

    finish all

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,28 @@
 package tw.core;
 
 
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
 
+    @Test(expected = IllegalArgumentException.class)
+    public void text_generateNums_should_return_IllegalArgumentException() throws Exception{
+
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        randomIntGenerator.generateNums(4,9);
+    }
+
+    @Test
+    public void text_generateNums_should_return_randomarray() throws Exception{
+
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+
+        assertThat(randomIntGenerator.generateNums(9,4).length()).isEqualTo(7);
+    }
 }
