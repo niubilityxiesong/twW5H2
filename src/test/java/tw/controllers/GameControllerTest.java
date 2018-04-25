48afc2c80d6245ce5d2d57c71190d1fa5229bca6
niubilitisong <66543775xie>
2018-04-25 15:12:14 +0800

    add mock test

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -2,11 +2,16 @@ package tw.controllers;
 
 import com.google.inject.Injector;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import tw.GuessNumberModule;
 import tw.commands.GuessInputCommand;
+import tw.commands.InputCommand;
 import tw.core.Answer;
 import tw.core.Game;
+import tw.core.exception.OutOfRangeAnswerException;
 import tw.core.generator.AnswerGenerator;
 import tw.core.generator.RandomIntGenerator;
 import tw.views.GameView;
@@ -15,6 +20,10 @@ import static com.google.inject.Guice.createInjector;
 import static junit.framework.TestCase.assertEquals;
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 import java.io.*;
 
@@ -60,5 +69,54 @@ public class GameControllerTest {
         assertTrue(output.contains("Game Status:"));
     }
 
+    @Mock
+    AnswerGenerator answerGenerator;
+    @Mock
+    Game game;
+    @Mock
+    GameView gameView;
+    @Mock
+    InputCommand inputCommand;
+
+    @Before
+    public void setup() throws Exception{
+
+        MockitoAnnotations.initMocks(this);
+        when(answerGenerator.generate()).thenReturn(Answer.createAnswer("1 2 3 4"));
+        game = new Game(answerGenerator);
+    }
+
+
+
+    @Test
+    public void showGameStatus_should_return_true() throws Exception{
+        GameController gameController = new GameController(game, gameView);
+        Answer answer = Answer.createAnswer("1 2 3 4");
+        when(inputCommand.input()).thenReturn(answer);
+
+        gameController.play(inputCommand);
+        verify(gameView).showGameStatus("success");
+
+    }
+
+    @Test
+    public void showGameStatus_should_return_false() throws Exception{
+
+        GameController gameController = new GameController(game, gameView);
+        Answer answer1 = Answer.createAnswer("1 2 3 5");
+        Answer answer2 = Answer.createAnswer("1 4 3 5");
+        Answer answer3 = Answer.createAnswer("1 6 3 5");
+        Answer answer4 = Answer.createAnswer("1 7 3 5");
+        Answer answer5 = Answer.createAnswer("1 8 3 5");
+        Answer answer6 = Answer.createAnswer("1 2 3 7");
+
+        when(inputCommand.input()).thenReturn(answer1).thenReturn(answer2).thenReturn(answer3).thenReturn(answer4)
+        .thenReturn(answer5).thenReturn(answer6);
+
+        gameController.play(inputCommand);
+        verify(gameView).showGameStatus("fail");
+        verify(gameView, times(6)).showGuessResult(any());
+
+    }
 
 }
